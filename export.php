3711f82ad01b9b71013b53e8ea6afa8879fcfcbf
brantje <brantje@gmail.com>
2014-06-29 12:26:18 +0200

    Final release

--- export.php
+++ export.php
@@ -1,19 +1,29 @@
 <?php
 require_once('functions.php');
 $bdd = teampass_connect();
-$response = $bdd->query("SELECT i.id, i.label, i.id_tree, i.pw AS 
-pw , i.url, i.login, i.description, i.email, t.id AS treeid, t.title AS foldertitle, t.parent_id as folder_parent
-FROM teampass_items AS i
-JOIN  `teampass_nested_tree` AS t ON  `i`.`id_tree` =  `t`.`id`");
+
+
+$tree = "SELECT a.*, b.title as parent_title FROM `".$GLOBALS['pre']."nested_tree` AS a LEFT JOIN  `".$GLOBALS['pre']."nested_tree` AS b ON  `a`.`parent_id` =  `b`.`id`";
+$response = $bdd->query($tree);
+$folders = array();
+while ($data = $response->fetch())
+{
+	$folders[] = $data;
+	echo "Folder: ".utf8_encode($data['title'])."\n";
+} 
+
+$response = $bdd->query("SELECT a.*,b.title as folder_title FROM `".$GLOBALS['pre']."items` AS a LEFT JOIN  `".$GLOBALS['pre']."nested_tree` AS b ON  `a`.`id_tree` =  `b`.`id`;");
 $rand_key = teampass_get_randkey();
+$items = array();
 while ($data = $response->fetch())
 {
 	$id = $data['id'];
 	$data['label'] = utf8_encode($data['label']);
 	echo utf8_encode($data['label'])."\n";
 	$data['login'] = utf8_encode($data['login']);
-	$data['pw'] = teampass_decrypt_pw($data['pw'],SALT,$rand_key);
-	$json[] = $data;
+	$data['pw'] = teampass_decrypt_pw($data['pw'],SALT,$rand_key); 
+	$items[] = $data;
 }
-
+$json['items'] = $items;
+$json['folders'] = $folders;
 file_put_contents('passwords.json',json_encode($json));
